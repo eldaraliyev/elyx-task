23702dba1bc08423173406984a8b16a9c9b0b1b4
Eldar <eldar.alv@gmail.com>
2022-04-23 01:19:56 +0400

    Commit

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# Build project
+
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# commit all changes
+
+git init
+git add dist
+git commit -m 'adding dist subtree'
+
+# push all changes on deployment subtree 
+
+git subtree push --prefix dist origin gh-pages
--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,5 @@ module.exports = {
     }
   },
   publicPath: process.env.NODE_ENV === 'production' ?
-    '/vue-todo-app/' : '/'
+    '/elyx-task/' : '/'
 };
