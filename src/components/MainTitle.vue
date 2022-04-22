17c38a4ecdf4eccbdf0a44dcd04cdee3176f3e51
Eldar <eldar.alv@gmail.com>
2022-04-23 00:43:02 +0400

    first-commit

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="main-title">
+    <div class="left-section">
+      <p class="title">{{signUp ?'Sign up' : 'Sign in'}} to</p>
+      <p class="subtitle">Lorem Ipsum is simply</p>
+      <p class="description">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </p>
+    </div>
+    <div class="right-section">
+      <img src="../assets/img/main.png" alt="" />
+    </div>
+  </div>
+</template>
+<script>
+import {signUp} from '../api'
+export default {
+  setup() {
+    return {signUp};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main-title {
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 60%;
+  position: relative;
+  .left-section {
+    padding-top: 150px;
+    margin-right: -40px;
+  }
+  .right-section img {
+    margin-top: 25px;
+    margin-right: -12px;
+  }
+  .title {
+    font-size: 34px;
+    font-weight: 600;
+    line-height: 51px;
+  }
+  .subtitle {
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 36px;
+  }
+  .description {
+    font-size: 13px;
+    font-weight: 300;
+    line-height: 20px;
+    margin-top: 22px;
+  }
+}
+@media screen and (max-width: 1024px) {
+  .left-section {
+    padding-right: 30vw;
+
+  }
+  .right-section img {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .main-title {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/UI/AppButton.vue
@@ -0,0 +1,70 @@
+<template>
+  <button :class="className">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import { toRef } from "@vue/reactivity";
+export default {
+  props: {
+    class: {
+      type: String,
+      required: false,
+      default: "main",
+    },
+  },
+  setup(props) {
+    const className = toRef(props, "class");
+    return {
+      className,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  display: flex;
+  justify-content: center;
+  flex: 2 1 auto;
+  align-items: center;
+  border: none;
+  padding: 12px 15px;
+  width: max-content;
+  border-radius: 10px;
+  height: 55px;
+  cursor: pointer;
+}
+.google {
+  background: #e9f1ff;
+  color: #4285f4;
+  display: flex;
+  gap: 21px;
+  padding: 32px auto;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 12px;
+    line-height: 18px;
+    gap: 12px;
+   }
+
+}
+
+.main {
+  background: $btn-color;
+  width: 15%;
+  max-width: 60px;
+}
+.sign-in {
+  width: 100%;
+  background: $main-color;
+  color: white;
+  margin-top: 50px;
+
+  box-shadow: $button-shadow;
+}
+</style>
